0eacc3cbafdc0da3811c607219ad3ad857c5fbc2
nelsaj <112635402+nelsaj@users.noreply.github.com>
2023-02-16 12:53:58 +0100

    progress on login_register

--- js/login_register.js
+++ js/login_register.js
@@ -1,6 +1,11 @@
 document.querySelector("#wrapper").classList.add("coral");
 document.querySelector("#wrapper").innerHTML = `
-    <div id="infoBox">hej</div>
+    <div id="whiteScreen" class="hide">    
+        <div id="infoBox">
+            <div></div>
+            <button class="hide">CLOSE</button>
+        </div>
+    </div>
 
     <header>
         <div class="logo"></div>
@@ -32,25 +37,86 @@ document.querySelector("#wrapper").innerHTML = `
     </footer>
 `;
 
-document.querySelector("#button button").addEventListener("click", e => {
-    if (document.querySelector("#name").value === "hey") {
-        console.log("yay");
-    }
-})
+// variabler för lättare läsning
+const username = document.querySelector("#name");
+const password = document.querySelector("#pass");
 
-document.querySelector("#loginRegister").addEventListener("click", e => {
+const button = document.querySelector("#button button");
+const loginOrRegister = document.querySelector("#loginRegister");
+
+const currentPage = document.querySelector("#current_page");
+const flavorText = document.querySelector("#button label");
+const inputs = document.querySelectorAll("input");
+
+const whiteScreen = document.querySelector("#whiteScreen");
+const infoBox = document.querySelector("#infoBox");
+const infoBoxText = document.querySelector("#infoBox > div");
+const infoBoxButton = document.querySelector("#infoBox > button");
+
+button.addEventListener("click", beginLoginRegister);
+
+async function beginLoginRegister() {
+    if (document.querySelector("#wrapper").classList.contains("darkCoral")) {
+        const newUser = {
+            action: "register",
+            user_name: username.value,
+            password: password.value,
+        }
+        const POSTNewUser = {
+            method: "POST",
+            headers: { "Content-type": "application/json; charset=UTF-8" },
+            body: JSON.stringify(newUser)
+        }
+        const request = new Request(`https://teaching.maumt.se/apis/access/`, POSTNewUser);
+        infoBoxContent("Contacting server");
+        const fetched = await fetch_request(request);
+        if (fetched.status === 200) {
+            infoBoxContent("New user created!", true);
+        } else if (fetched.status === 409) {
+            infoBoxContent("Sorry that user is already taken", true);
+        }
+    } else {
+        const request = new Request(`https://teaching.maumt.se/apis/access/?action=check_credentials&user_name=${username.value}&password=${password.value}`);
+        infoBoxContent("Contacting server");
+        const fetched = await fetch_request(request);
+        if (fetched.status === 200) {
+            infoBoxContent("Logged in!");
+        } else if (fetched.status === 404) {
+            flavorText.textContent = "Username or password incorrect";
+            flavorText.style.backgroundColor
+        }
+    }
+}
+
+loginOrRegister.addEventListener("click", e => {
+    for (const input of inputs) { input.value = ""; }
     document.querySelector("#wrapper").classList.toggle("coral");
     document.querySelector("#wrapper").classList.toggle("darkCoral");
 
     if (document.querySelector("#wrapper").classList.contains("darkCoral")) {
-        document.querySelector("#current_page").textContent = "REGISTER";
-        document.querySelector("#button label").textContent = "Ready when you are...";
-        document.querySelector("#loginRegister").textContent = "Already have an account? Go to login";
-        document.querySelector("#button button").textContent = "Register";
+        currentPage.textContent = "REGISTER";
+        flavorText.textContent = "Ready when you are...";
+        loginOrRegister.textContent = "Already have an account? Go to login";
+        button.textContent = "Register";
     } else {
-        document.querySelector("#current_page").textContent = "LOGIN";
-        document.querySelector("#button label").textContent = "Let the magic start!";
-        document.querySelector("#loginRegister").textContent = "New to this? Register for free.";
-        document.querySelector("#button button").textContent = "Login";
+        currentPage.textContent = "LOGIN";
+        flavorText.textContent = "Let the magic start!";
+        loginOrRegister.textContent = "New to this? Register for free.";
+        button.textContent = "Login";
     }
 })
+
+infoBoxButton.addEventListener("click", infoBoxRemove);
+
+function infoBoxContent(content, button) {
+    infoBoxText.textContent = content;
+    whiteScreen.classList.remove("hide");
+    if (button) {
+        infoBoxButton.classList.remove("hide");
+    }
+}
+
+function infoBoxRemove() {
+    whiteScreen.classList.add("hide");
+    infoBoxButton.classList.add("hide");
+}
